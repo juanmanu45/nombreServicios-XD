9431c943d476dba1ba21a7b57270e3fdfd785f5c
juanmanu45 <juanmanu45@gmail.com>
2019-09-27 10:14:33 -0500

    1 file modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { PeopleComponent } from './people/people.component';
-import { CalculadoraService } from './people/Servicios/calculadora.service';
-import { RestaService } from './people/Servicios/resta.service';
-import { MultiService } from './people/Servicios/multi.service';
+import { CalculadoraComponent } from './ejer2/calculadora/calculadora.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, PeopleComponent ],
+  declarations: [ AppComponent, HelloComponent, PeopleComponent, CalculadoraComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [CalculadoraService, RestaService, MultiService]
+  
   
 })
 export class AppModule { }
