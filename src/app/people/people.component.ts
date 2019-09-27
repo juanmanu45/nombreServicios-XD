f6a412dab902e6a9f775d0adea364d5ba27eb1ca
juanmanu45 <juanmanu45@gmail.com>
2019-09-27 09:56:49 -0500

    2 files modified, 1 added

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FormsModule } from '@angular/forms';
 @Component({
-  selector: 'app-people',
-  templateUrl: './people.component.html',
-  styleUrls: ['./people.component.css']
+  selector: "app-people",
+  templateUrl: "./people.component.html",
+  styleUrls: ["./people.component.css"]
 })
 export class PeopleComponent implements OnInit {
+  nombre: string;
+  apellido: string;
+  saludo: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  saludar(): void {
+    this.saludo = "hola";
   }
-
 }
